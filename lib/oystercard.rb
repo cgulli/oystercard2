c86740e08676975bd63d6cf2640cc3af26ddede8
Caitlin <caitlin.gulliford@gmail.com>
2016-10-04 11:25:12 +0100

    challenge 7 deduct far

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,13 +2,17 @@ class Oystercard
  attr_reader :balance, :max_balance
  MAX_BALANCE = 90
 
- def initialize(max_balance = MAX_BALANCE)
-   @balance = 0
-   @max_balance = max_balance
+ def initialize(balance = 0)
+   @balance = balance
+   @max_balance = MAX_BALANCE
  end
 
  def top_up(top_up_value)
-   fail 'Maximum balance of 90 exceded' if balance > max_balance
+   raise "Maximum balance of 90 exceeded" if top_up_value + @balance > @max_balance
    @balance += top_up_value
+ end
+
+ def deduct(top_up_value)
+   @balance -= top_up_value
  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,13 +11,20 @@ describe Oystercard do
    it { is_expected.to respond_to(:top_up).with(1).argument }
 
    it 'can top up the balance' do
-     top_up_value = 5
-     expect{ oystercard.top_up(top_up_value) }.to change{ oystercard.balance }.by top_up_value
+     expect{ oystercard.top_up(0) }.to change{ oystercard.balance }.by 0
    end
 
    it 'enforces maximum balance' do
      oystercard.top_up(Oystercard::MAX_BALANCE)
-     expect{ oystercard.top_up(100) }.to raise_error 'Maximum balance of 90 exceded'
+     expect{ oystercard.top_up(100) }.to raise_error "Maximum balance of 90 exceeded"
    end
  end
-end
+
+ describe '#deduct'do
+   it { is_expected.to respond_to(:deduct).with(1).argument}
+
+   it 'reduces balance by one fare amount' do
+    expect{ oystercard.deduct(2) }.to change{ oystercard.balance }.by (-2)
+   end
+ end
+ end
