4c8c8d31ff5068b6389832f83fa3e77f04fa5560
Caitlin <caitlin.gulliford@gmail.com>
2016-10-06 17:41:11 +0100

    created journey class properly

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,14 +1,35 @@
 require_relative 'station'
 
 class Journey
-  attr_accessor :journey
-  
-  def initialize(start_station)
-    @journey = [start_station]
+
+MIN_FARE = 1
+PEN_FARE = 6
+attr_reader :start_station, :end_station
+
+  def start(station)
+    @start_station = station
   end
 
   def end(station)
-    @journey[1] = station
+    @end_station = station
   end
 
+  def calculate_fare
+    completed? ? MIN_FARE : PEN_FARE
+  end
+
+private
+  def completed?
+    @start_station && @end_station
+  end
+
+
 end
+
+
+
+
+
+# def initialize(start_station)
+#   @journey = [start_station]
+# end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,23 +2,38 @@ require 'journey'
 
 describe Journey do
 
-  subject(:journey) { described_class.new(start_station) }
+subject(:journey) { described_class.new }
+let(:station) { double(:angel_station)}
 
-  let(:start_station) {double :start_station}
-  let(:end_station) {double :end_station}
-
-  it 'initializes with a start station' do
-    expect(subject.journey).to eq [start_station]
+describe '#start' do
+  it 'sets an entry station' do
+    journey.start(station)
+    expect(journey.start_station).to eq station
   end
+end
 
   describe '#end' do
     it "ends a journey" do
-      journey.end(end_station)
-      expect(journey.journey[1]).to eq end_station
+      journey.end(station)
+      expect(journey.end_station).to eq station
     end
   end
 
+describe '#calculate fare' do
+  it 'charges a minimum fare if journey is completed' do
+      journey.start(station)
+      journey.end(station)
+      expect(journey.calculate_fare).to eq Journey::MIN_FARE
+    end
 
+  it 'charges a penalty fare if journey does not have a start' do
+    journey.end(station)
+    expect(journey.calculate_fare).to eq Journey::PEN_FARE
+    end
 
-
+  it 'charges a penalty fare if journey does not have an end' do
+    journey.start(station)
+    expect(journey.calculate_fare).to eq Journey::PEN_FARE
+    end
+  end
 end
